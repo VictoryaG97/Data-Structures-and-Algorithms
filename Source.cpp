e6e389932fcc14d8d773227a233457a90dfc2733
VictoryaG97 <victorya.guncheva@abv.bg>
2017-11-10 13:39:47 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int Fib(int n)
+{
+	if (n <= 1)
+		return n;
+	return Fib(n - 1) + Fib(n - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	cout << Fib(n) << endl;
+
+	return 0;
+}
